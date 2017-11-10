fcb63c5255012933c1728c4c78c196932cd3524b
anakin14 <anakin14@users.noreply.github.com>
2017-11-10 13:28:31 -0500

    gui 1.1

--- src/EnigmaGUI.java
+++ src/EnigmaGUI.java
@@ -9,11 +9,16 @@ import javax.swing.SwingConstants;
 import java.awt.Color;
 import javax.swing.JTextField;
 import java.awt.Font;
+import javax.swing.JPanel;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import java.awt.FlowLayout;
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+import java.awt.Component;
 
 public class EnigmaGUI {
 
 	private JFrame Welcome_Frame;
-	private JTextField txtWelcomeToEnigma;
 
 	/**
 	 * Launch the application.
@@ -42,34 +47,45 @@ public class EnigmaGUI {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		int x = 800, y = 750;   //x is width, y is height
 		Welcome_Frame = new JFrame();
-		Welcome_Frame.setBounds(100, 100, 450, 300);
+		Welcome_Frame.setBounds(0, 0, x+20, y);
 		Welcome_Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Welcome_Frame.getContentPane().setLayout(null);
 		
-		JButton default_Settings = new JButton("Default settings");
-		default_Settings.setToolTipText("Use the preset configuration");
-		default_Settings.setBackground(Color.CYAN);
-		default_Settings.setHorizontalAlignment(SwingConstants.LEFT);
-		default_Settings.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		Welcome_Frame.getContentPane().add(default_Settings, BorderLayout.WEST);
+		JPanel panel = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+		flowLayout.setVgap(45);
+		flowLayout.setHgap(0);
+		panel.setBounds(0, 0, 800, 102);
+		Welcome_Frame.getContentPane().add(panel);
 		
-		txtWelcomeToEnigma = new JTextField();
-		txtWelcomeToEnigma.setFont(new Font("Tahoma", Font.BOLD, 15));
-		txtWelcomeToEnigma.setText("Welcome to Enigma Emulator!");
-		Welcome_Frame.getContentPane().add(txtWelcomeToEnigma, BorderLayout.CENTER);
-		txtWelcomeToEnigma.setColumns(10);
+		JLabel lblNewLabel = new JLabel("Enigma Emulator");
+		lblNewLabel.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 32));
+		panel.add(lblNewLabel);
 		
-		JButton btnCustumSettings = new JButton("Custum Settings");
-		btnCustumSettings.setSize(null);
-		btnCustumSettings.addActionListener(new ActionListener() {
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(417, 137, 343, 459);
+		Welcome_Frame.getContentPane().add(panel_2);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(170, 280, 10, -59);
+		Welcome_Frame.getContentPane().add(panel_1);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBounds(40, 137, 334, 459);
+		Welcome_Frame.getContentPane().add(panel_3);
+		
+		JButton btnNewButton = new JButton("Default Settings");
+		panel_3.add(btnNewButton);
+		btnNewButton.setBackground(new Color(0, 191, 255));
+		Welcome_Frame.getContentPane().setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{panel, lblNewLabel, panel_2, btnNewButton, panel_1}));
+		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnCustumSettings.setToolTipText("Edit the Settings");
-		Welcome_Frame.getContentPane().add(btnCustumSettings, BorderLayout.EAST);
+	
 	}
-
 }
